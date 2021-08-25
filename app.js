a835a72901367b2a4aa335b9612622c12614177a
jannat456nur <jannatjenny22@gmail.com>
2021-08-24 21:25:44 -0700

    third functon add

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 
+function getInputValue(inputId) {
+    const inputFeild = document.getElementById(inputId);
+    const inputAmountText = inputFeild.value;
+    inputFeild.value = '';
+    return inputAmountText;
+
+}
+
+function updatetotalfield(totalFeildId, depositInputText) {
+    const depositTotal = document.getElementById(totalFeildId);
+    const depositTotalText = depositTotal.innerText;
+    depositTotal.innerText = parseFloat(depositTotalText) + parseFloat(depositInputText);
+    return depositTotalText;
+
+}
+
 document.getElementById('deposit-button').addEventListener('click', function () {
     // console.log('ghfhghn')
-    const depositInput = document.getElementById('deposit-input');
-    const depositInputText = depositInput.value;
+    // const depositInput = document.getElementById('deposit-input');
+    // const depositInputText = depositInput.value;
     // console.log(depositInputText)
+    const depositInputText = getInputValue('deposit-input');
 
     //get current deposit
-    const depositTotal = document.getElementById('deposit-total');
-    const depositTotalText = depositTotal.innerText;
+    // const depositTotal = document.getElementById('deposit-total');
+    // const depositTotalText = depositTotal.innerText;
+    updatetotalfield('deposit-total', depositInputText)
     // console.log(depositTotalText)
-    depositTotal.innerText = parseFloat(depositTotalText) + parseFloat(depositInputText);
+    // depositTotal.innerText = parseFloat(depositTotalText) + parseFloat(depositInputText);
 
     //update baalance2
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
+    // const balanceTotal = document.getElementById('balance-total');
+    // const balanceTotalText = balanceTotal.innerText;
     // console.log(balanceTotalText)
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-    balanceTotal.innerText = previousBalanceTotal + parseFloat(depositInputText);
+    // const previousBalanceTotal = parseFloat(balanceTotalText);
+    // balanceTotal.innerText = previousBalanceTotal + parseFloat(depositInputText);
 
-    depositInput.value = '';
+    updatetotalfield('balance-total', depositInputText)
 
 
 });
@@ -26,19 +44,20 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
     // console.log('sfgsdhf')
-    const withdrawInput = document.getElementById('Withdraw-input');
-    const withdrawInputText = withdrawInput.value;
+    // const withdrawInput = document.getElementById('Withdraw-input');
+    // const withdrawInputText = withdrawInput.value;
+    const withdrawInputText = getInputValue('Withdraw-input')
     // console.log(withdrawInputText)
     const withdrawTotal = document.getElementById('withdraw-total');
     const withdrawTotalText = withdrawTotal.innerText;
     // console.log(withdrawTotalText)
     withdrawTotal.innerText = parseFloat(withdrawInputText) + parseFloat(withdrawTotalText);
-    withdrawInput.value = '';
+    // withdrawInput.value = '';
     //update diposit
 
     const drawTotal = document.getElementById('balance-total');
     const drawTotalText = drawTotal.innerText;
     const text = parseFloat(drawTotalText);
-    drawTotal.innerText = parseFloat(withdrawInputText) - text;
+    drawTotal.innerText = text - parseFloat(withdrawInputText);
 
 })
